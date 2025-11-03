b7ef1abe5b4f96545b5943848e311cf638c520cc
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-11-03 12:39:10 +0300

    🥳🥳🥳post request endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,32 @@
-import express from 'express';
+import express from "express";
 
- const router = express.Router();
+const router = express.Router();
+const user = [
+  {
+    firstName: "Alf",
+    lastName: "Yano",
+    age: 25,
+  },
+   {
+    firstName: "Jay",
+    lastName: "Robbin",
+    age: 15,
+  },
+];
 
- // all routes are starting with "/users"
- // this will be "/users/"
- router.get('/',(req,res)=>{
-    res.send('These are the users');
- }); 
+// all routes are starting with "/users"
+// this will be "/users/"
+router.get("/", (req, res) => {
+    console.log(user);
+  res.json(user);
+});
 
+router.post("/",(req,res)=>{
+    const n_user = req.body
+    console.log(n_user);
+    user.push(n_user)
+    
+    res.json(n_user);
+});
 // make it availabe for use in other files
- export default router;
+export default router;
