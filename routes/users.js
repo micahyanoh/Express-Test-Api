8f25feb1d6be78bed19713a37f318ba5558e6788
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-11-03 13:57:48 +0300

    🥳🥳🥳🥳

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const user = [
     lastName: "Yano",
     age: 25,
   },
-   {
+  {
     firstName: "Jay",
     lastName: "Robbin",
     age: 15,
@@ -17,16 +17,16 @@ const user = [
 // all routes are starting with "/users"
 // this will be "/users/"
 router.get("/", (req, res) => {
-    console.log(user);
+  console.log(user);
   res.json(user);
 });
 
-router.post("/",(req,res)=>{
-    const n_user = req.body
-    console.log(n_user);
-    user.push(n_user)
-    
-    res.json(n_user);
+router.post("/", (req, res) => {
+  const n_user = req.body;
+  console.log(n_user);
+  user.push(n_user);
+
+  res.json(n_user);
 });
 // make it availabe for use in other files
 export default router;
